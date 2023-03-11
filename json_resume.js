790a88b22f320a41ee3d9f9040c60b2207e49958
kokilaAravindhan <125110809+kokilaAravindhan@users.noreply.github.com>
2023-03-12 00:37:58 +0530

    Add files via upload

---
+++ json_resume.js
@@ -0,0 +1,14 @@
+var resume=
+{"Employee":
+    {
+       "name":"XXXX",
+       "ID":"0001",
+       "Dept":"IT",
+       "name":"yyyy",
+       "ID":"0002",
+       "Dept":"Networking",
+       "name":"HHHH",
+       "ID":"0003",
+       "Dept":"Accountant"
+    }
+}
